1bda0f4c02c455ee3bc0e772fac4b8a2a38ad32e
Suchita Vader <suchitavader18@gmail.com>
2020-07-08 11:01:36 +0530

    New added commit

---
+++ forloop.py
@@ -0,0 +1,10 @@
+for i in range(1,11):
+    print("Hello World")
+
+for i in range(10):
+    print(f"Hello World :{i}")
+
+for i in range(10):
+    print(f"Hello World :{i}")
+    print("This is line \n")
+
